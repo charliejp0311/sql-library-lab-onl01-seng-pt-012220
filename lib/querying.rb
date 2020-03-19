6bd2ee2da183dc75397f659efa7e380243c2ba2d
Charles Pelton <charliejp0311@gmail.com>
2020-03-19 22:32:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,7 @@ def select_series_title_with_most_human_characters
     INNER JOIN character_books ON Books.id = character_books.book_id
     INNER JOIN Characters ON character_books.character_id = Characters.id
     WHERE Characters.species = 'human'
-    GROUP BY Series.title ; "
+    GROUP BY Series.title LIMIT 1; "
 end
 
 def select_character_names_and_number_of_books_they_are_in
